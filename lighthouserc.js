1b8eec697eaebd9bf2a530fc013208025420cb78
Vicente Opaso <vicente@opa.so>
2025-11-28 13:38:31 +0000

    fix: Raise best-practices threshold to 0.9
    Addresses PR review comments:
    - https://github.com/vicenteopaso/vicenteopaso-vibecode/pull/62#discussion_r2571522744
    - https://github.com/vicenteopaso/vicenteopaso-vibecode/pull/62#discussion_r2571573415

--- lighthouserc.js
+++ lighthouserc.js
@@ -34,7 +34,7 @@ module.exports = {
       assertions: {
         "categories:performance": ["warn", { minScore: 0.9 }],
         "categories:accessibility": ["warn", { minScore: 0.9 }],
-        "categories:best-practices": ["warn", { minScore: 0.7 }],
+        "categories:best-practices": ["warn", { minScore: 0.9 }],
         "categories:seo": ["error", { minScore: 0.95 }],
         // Additional specific assertions for critical metrics
         "first-contentful-paint": ["warn", { maxNumericValue: 2000 }],
