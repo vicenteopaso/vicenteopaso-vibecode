882fbafeb6b210e8feb5db730540c033c957bdc0
Vicente Opaso <vicente@opa.so>
2026-06-07 15:34:21 +0200

    Update sitemap-0.xml with new lastmod timestamps for all URLs

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -22,5 +22,14 @@ module.exports = {
   generateRobotsTxt: false,
   sitemapSize: 5000,
   changefreq: "monthly",
-  exclude: ["/opengraph-image", "/*/opengraph-image", "/twitter-image", "/*/twitter-image", "/icon", "/*/icon", "/apple-icon", "/*/apple-icon"],
+  exclude: [
+    "/opengraph-image",
+    "/*/opengraph-image",
+    "/twitter-image",
+    "/*/twitter-image",
+    "/icon",
+    "/*/icon",
+    "/apple-icon",
+    "/*/apple-icon",
+  ],
 };
